08a5b06527fcb4da559b1efeffded3768b85dac5
Tom Beckmann <tomjonabc@gmail.com>
2017-12-26 02:17:17 +0100

    add player names next to health bars

--- game/hud/hud.gd
+++ game/hud/hud.gd
@@ -1,28 +1,36 @@
 extends CanvasLayer
 
-var hitpoint_bars = {}
-var players
+const PLAYER_NAME_MAX_WIDTH = 70
 
-func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	pass
+var players
 
 func set_players(p):
 	players = p
-	
+
 	var y_offset = 0
 	for p_id in players:
+		var bar_group = Node2D.new()
+		bar_group.position.y = y_offset
+		bar_group.set_name(str(p_id))
+
+		var label = Label.new()
+		label.text = players[p_id].player_name
+		label.rect_clip_content = true
+		label.rect_size.x = PLAYER_NAME_MAX_WIDTH
+
 		var bar = ProgressBar.new()
+		bar.set_name("health")
 		bar.rect_size = Vector2(150, 16)
-		bar.rect_position.y = y_offset
+		bar.rect_position.x = PLAYER_NAME_MAX_WIDTH
 		bar.max_value = players[p_id].MAX_HITPOINTS
 		bar.min_value = 0
+
 		y_offset += 20
-		$health_bars.add_child(bar)
-		hitpoint_bars[p_id] = bar
+
+		bar_group.add_child(label)
+		bar_group.add_child(bar)
+		$bars.add_child(bar_group)
 
 func update_bars():
 	for p_id in players:
-		print("HITPOINTS: " + str(players[p_id].hitpoints))
-		hitpoint_bars[p_id].value = players[p_id].hitpoints
+		get_node("bars/" + str(p_id) + "/health").value = players[p_id].hitpoints
